9b6f7cc22c191b84fd2d496bdc0463b1824b02c5
Daniel Li <101207085+newtricksterx@users.noreply.github.com>
2024-10-22 01:24:03 -0400

    cleaned up code

--- frontend/src/app/(routes)/issues/page.tsx
+++ frontend/src/app/(routes)/issues/page.tsx
@@ -1,30 +1,6 @@
-//import { useFetchQuerySet } from "@/app/CustomHooks/useFetchQuerySet";
-import { Issue, columns } from "./columns"
-import { DataTable } from "./data-table"
 import TablePage from "./table-page";
-/*
-async function getData(): Promise<Issue[]> {
-  // Fetch data from your API here.
-  return [
-    {
-      id: 1,
-      title: "b",
-      status: "COMPLETED",
-      priority: "NORMAL",
-    },
-    {
-      id: 2,
-      title: "a",
-      status: "COMPLETED",
-      priority: "NORMAL",
-    },
-    // ...
-  ]
-}*/
 
-export default function DemoPage() {
-  //const data = await getData()
-  //const data = useFetchQuerySet<Issue>('api/epic/');
+export default function IssuesPage() {
   return (
     <TablePage />
   )
