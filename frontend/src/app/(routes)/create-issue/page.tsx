368f0b8b7539e10714faa0cfc7324ae39b02c19f
Daniel Li <101207085+newtricksterx@users.noreply.github.com>
2024-10-23 13:02:15 -0400

    made modifications to previos commit

--- frontend/src/app/(routes)/create-issue/page.tsx
+++ frontend/src/app/(routes)/create-issue/page.tsx
@@ -3,8 +3,7 @@ import { CreateIssue } from "@/app/components/CreateIssue"
 
 export default function CreateIssuePage(){
     return (
-        <div>
-            <CreateIssue />
-        </div>
+
+        <CreateIssue />
     );
 }
